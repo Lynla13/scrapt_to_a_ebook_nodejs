ac7b5969e7385df500e76d25c353376386f5cacd
Lynla_13 <lynla1318@gmail.com>
2024-04-11 04:20:14 +0700

    Finnish get lightnovel function, add some new works to todo list

--- src/Controllers/API/Webs/Docln.ts
+++ src/Controllers/API/Webs/Docln.ts
@@ -12,23 +12,30 @@ class Docln {
     public static async getBook(req:Request, res: Response) {
 
         const query: string = req.body.query;
+        const searchLink : string = `https://docln.net/tim-kiem?keywords=${query}%3F`;
+        const getLNLink : string = '.series-title';
+        const getTitle : string = ".series-name a";
+        const getSumary : string  = ".summary-content";
+        const getAuthor : string = ".info-value a";
+        const getChapter : string =  ".chapter-name";
+        const getChapterContent : string = "#chapter-content";
         //Config pupperteer here
         const browser = await puppeteer.launch({headless: false, args: ['--disable-features=site-per-process']});
 		const page = await browser.newPage(); 
         await page.setViewport({ width: 1280, height: 720 });
-        await page.goto(Puppeteer.proxy (`https://docln.net/tim-kiem?keywords=${query}%3F`), {waitUntil: 'networkidle2'}); 
+        await page.goto(Puppeteer.proxy (`${searchLink}`), {waitUntil: 'networkidle2'}); 
         //Get link and covert to usable link
-        const href = await page.$eval(".series-title a", (elm) => elm.href);
+        const href = await page.$eval(`${getLNLink} a`, (elm) => elm.href);
         // Puppeteer.replaceLink (href : Link need to convert, link: base link)
         const subLink : string = Puppeteer.replaceLink (href, link)
         // Goto link
         await page.goto(subLink, {waitUntil: 'networkidle2'}); 
         Puppeteer.delay (400);
-        const title: any = await page.$eval(".series-name a", (elm) => elm.textContent);
-        const sumary: any = await page.$eval(".summary-content", (elm) => elm.textContent);
-        const author : any = await page.$eval (".info-value a", (elm) => elm.textContent);
+        const title: any = await page.$eval(`${getTitle}`, (elm) => elm.textContent);
+        const sumary: any = await page.$eval(`${getSumary}`, (elm) => elm.textContent);
+        const author : any = await page.$eval (`${getAuthor}`, (elm) => elm.textContent);
         // Get raw link
-        const sample = await page.$$eval(".chapter-name a", (list) => list.map((elm) => elm.href));
+        const sample = await page.$$eval(`${getChapter} a`, (list) => list.map((elm) => elm.href));
         //SaveBook to database
         Book.insertBook (title, sample.length, author,sumary);
         //Save chapter
@@ -38,7 +45,7 @@ class Docln {
            }
         for (let j = 0; j < chapterLink.length; j++) {
             await page.goto(Puppeteer.proxy (`${chapterLink [j]}`), {waitUntil: 'networkidle2'}); 
-            const content : any = await page.$eval ("#chapter-content", (elm) => elm.textContent);
+            const content : any = await page.$eval (`${getChapterContent}`, (elm) => elm.textContent);
             Chapter.insertChapter (title, j, content)
             Puppeteer.delay (400);
         } 
