8a346eea6b2b43c75aa82ce674dcb6ccda6c3695
Lynla_13 <lynla1318@gmail.com>
2024-04-12 13:31:48 +0700

    Add Notes

---
+++ src/Controllers/API/Webs/Blogtruyen.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import Puppeteer from "../Pupperteer";
+import fileSystem from "../Fs";
+
+const link : string = "https://docln.net";
+
+class BlogTruyen {
+  //get truyen moi 
+ //get truyen by tim kiem
+//Luu truyen vao sqlite de de tim 
+// Rotate về ip của mình
+
+    public static async getBook(req:Request, res: Response) {
+        const query: string = req.body.query;
+        const searchLink : string = `https://docln.net/tim-kiem?keywords=${query}%3F`;
+        const getBookLink : string = '.series-title';
+        const getTitle : string = ".series-name a";
+        const getSumary : string  = ".summary-content";
+        const getAuthor : string = ".info-value a";
+        const getChapter : string =  ".chapter-name";
+        const getChapterContent : string = "#chapter-content";
+        Puppeteer.getBook (link,searchLink,getBookLink,getTitle, getSumary, getAuthor, getChapter, getChapterContent)
+    }
+
+    static async toEpub (){
+
+    }
+}
+
+export default BlogTruyen ;
